78245b46f39c884e8a5078a2f2bf79c47c81ac8d
Marco <it.marco2807@gmail.com>
2023-11-22 10:24:03 +0100

    update

--- Server/Wiki/main.py
+++ Server/Wiki/main.py
@@ -13,12 +13,7 @@ app = Flask(__name__)
 
 @app.route("/test.html")
 def test():
-    import json
-    json_file_path = 'data.json'
-    with open(json_file_path, 'r') as file:
-        json_data = file.read()
-    data = json.loads(json_data)
-    return render_template("test.html", table_data=data) 
+    return render_template("test.html")
 
 @app.route("/links.html")
 def links():
